f11813e43c5e449512307da055f7b5054b81d112
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-11 16:03:11 -0600

    Added form redirect after submission

--- src/pages/meetups/new-meetup.tsx
+++ src/pages/meetups/new-meetup.tsx
@@ -1,10 +1,21 @@
 import NewMeetupForm from '@/components/meetups/NewMeetupForm';
 import { MeetupWithoutId } from '@/models/meetup';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function NewMeetup() {
-  function addMeetupHandler(enteredMeetupData: MeetupWithoutId) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData: MeetupWithoutId) {
+    const response = await fetch('/api/meetups', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
