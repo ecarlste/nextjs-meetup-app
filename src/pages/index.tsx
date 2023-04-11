c09d8fe87894d145467b985296a0835b1cf5647a
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-11 16:24:18 -0600

    Added db access/persist for meetup details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,18 @@
 import MeetupList from '@/components/meetups/MeetupList';
+import { prisma } from '@/lib/prisma';
 import { Meetup } from '@prisma/client';
 import React from 'react';
 
-const fakeMeetups: Meetup[] = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg/2560px-Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg',
-    address: '12345 Some Street, Some City, ST',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg/2560px-Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg',
-    address: '12345 Some Street, Some City, ST',
-    description: 'This is a second meetup!',
-  },
-];
-
-function HomePage() {
-  return <MeetupList meetups={fakeMeetups} />;
+function HomePage(props: { meetups: Meetup[] }) {
+  return <MeetupList meetups={props.meetups} />;
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const meetups = await prisma.meetup.findMany();
+
   return {
     props: {
-      meetups: fakeMeetups,
+      meetups: meetups,
     },
   };
 }
--- src/pages/meetups/[meetupId].tsx
+++ src/pages/meetups/[meetupId].tsx
@@ -1,34 +1,42 @@
 import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { prisma } from '@/lib/prisma';
 import { Meetup } from '@prisma/client';
 import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React, { Fragment } from 'react';
 
-const meetup: Meetup = {
-  id: 'm1',
-  title: 'A First Meetup',
-  image:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg/2560px-Istanbul_asv2021-10_img11_Y%C4%B1ld%C4%B1z_Hamidiye_Mosque.jpg',
-  address: '12345 Some Street, Some City, ST',
-  description: 'This is a first meetup!',
-};
-
-function MeetupDetails() {
-  return <MeetupDetail meetup={meetup} />;
+function MeetupDetails(props: { meetup: Meetup }) {
+  return <MeetupDetail meetup={props.meetup} />;
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-  const meetupId = context.params?.meetupId;
+  const meetupId = context.params?.meetupId as string;
 
   console.log(`meetupId: ${meetupId}`);
+  const meetup = await prisma.meetup.findFirstOrThrow({
+    where: { id: meetupId },
+  });
 
   return {
     props: { meetup },
   };
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const meetups = await prisma.meetup.findMany();
+
+  const paths = meetups.map((meetup) => {
+    return {
+      params: {
+        meetupId: meetup.id,
+      },
+    };
+  });
+
+  console.log('*** paths ***');
+  console.log(paths);
+
   return {
-    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+    paths,
     fallback: false,
   };
 }
